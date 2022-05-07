3d81b9b1118d457ac877d98d8eaef9a9b78142cd
Romcha Levedev <lr.mos@mail.ru>
2022-05-07 10:58:20 +0300

    ready

--- js/4-practice.js
+++ js/4-practice.js
@@ -58,7 +58,7 @@ console.log("Я - строка с \"двойными кавычками\" вну
 console.log("FirstLine\n\t\ SecondLine\nThirdLine");
 
 
-// ### 7. Задача 
+// ### 7. Задача +
 // Исправьте код двумя способами, чтобы он работал:
 // - экранирование;
 // - одинарные кавычки;
@@ -66,6 +66,13 @@ console.log("FirstLine\n\t\ SecondLine\nThirdLine");
 // ```
 // const myStr = "<a href="http://www.example.com" target="_blank">Link</a>";
 // ```
+
+// const myStr = `<a href="http://www.example.com" target="_blank">Link</a>"`;
+// const myStr2 = "\<a href=\"http://www.example.com\" target=\"_blank\">Link</a>\"";
+// console.log(myStr);
+// console.log(myStr2);
+
+
 
 // ### 8. Задача +
 // Пользуясь переменными выполнить склеивание: <br>
@@ -115,7 +122,7 @@ console.log(strText.length);
 
 
 
-// ### 13. Задача
+// ### 13. Задача +
 // В переменную tagline нужно записать строку "JustDoIt".
 // Используй s1, s2, s3 и конкатенацию строк.
 
@@ -123,16 +130,22 @@ console.log(strText.length);
 // - Для инициализации переменной tagline используй s1, s2, s3 и конкатенацию строк;
 // - Программа должна выводить в консоль значение переменной tagline;
 
-// ```
-// let s1 = "Do";
-// let s2 = "It";
-// let s3 = "Just";
-// ```
+let s1 = "Do";
+let s2 = "It";
+let s3 = "Just";
+let tagline = s3 + s1 + s2;
+console.log(tagline);
 
-// ### 14. Задача
+
+
+// ### 14. Задача +
 // Выполнить задачу 13 с использованием шаблонных литералов.
 
-// ### 15. Задача
+console.log(`${s3 + s1 + s2}`);
+
+
+
+// ### 15. Задача +
 // В переменную digits нужно записать строку "60". 
 // Используй переменные x, y, z, пустую строку.
 
@@ -140,37 +153,45 @@ console.log(strText.length);
 // - Для инициализации переменной digits используй x, y, z, пустую строку;
 // - Программа должна выводить в консоль переменную digits;
 
-// ```
+
 // let x = 2;
 // let y = 4;
 // let z = 0;
-// ```
+// let digits = x + y + String(z);
+// console.log(digits);
 
-// ### 16. Задача
+
+
+// ### 16. Задача + -
 // В переменную digits нужно записать строку "100". Используй переменные x, y, пустую строку.
 
 // - Не изменяй значения переменных x, y, z;
 // - Для инициализации переменной digits используй x, y, z, пустую строку;
 // - Программа должна выводить в консоль переменную digits;
 
-// ```
-// let x = 5;
-// let y = 4;
-// ```
+let x = 5;
+let y = 4;
+let z = 0;
+let digits = x + y + String(z);
+console.log(digits);
 
-// ### 17. Задача
+
+// ### 17. Задача +
 // В переменную hugeAmount нужно записать число 100500.
 // Используй переменные bigAmount, greatAmount и преобразование строки в число.
 
 // - Не изменяй значения переменных bigAmount и greatAmount;
 // - Программа должна выводить в консоль переменную hugeAmount;
 
-// ```
-// let bigAmount = "500";
-// let greatAmount = "100000";
-// ```
 
-// ### 18. Задача
+let bigAmount = "500";
+let greatAmount = "100000";
+let hugeAmount = Number(bigAmount) + Number(greatAmount);
+console.log(hugeAmount);
+
+
+
+// ### 18. Задача +
 // В консоль выводятся значения трех строк.
 // Внеси изменения в код, чтобы вместо значений строк вывелась длина каждой строки.
 // Не изменяй значение переменной emptyString.
@@ -182,18 +203,28 @@ console.log(strText.length);
 // console.log((emptyString + 4 + 3 + "11"));
 // ```
 
-// ### 19. Задача
+let emptyString = "";
+console.log(emptyString.length);
+console.log("Oh no!".length);
+console.log((emptyString + 4 + 3 + "11").length);
+
+// ### 19. Задача + -
 // В консоль выводится четыре строки.
 // Внеси изменения в код, чтобы все буквы в этих строках стали строчными.
 // Не изменяй значения переменных title, degree и career.
 
-// ```
-// let title = "Senior Lead Principal Software Engineer Data Architect";
-// let degree = "In college, I Majored in Political Science and Minored in Religious Studies.";
-// let career = "Experienced Team Leader with strong Organizational Skills and a Successful career in Management.";
-// ```
+let title = "Senior Lead Principal Software Engineer Data Architect";
+let degree = "In college, I Majored in Political Science and Minored in Religious Studies.";
+let career = "Experienced Team Leader with strong Organizational Skills and a Successful career in Management.";
 
-// ### 20. Задача
+console.log(title.toLowerCase());
+console.log(degree.toLowerCase());
+console.log(career.toLowerCase());
+
+
+
+
+// ### 20. Задача +
 // Считай с клавиатуры три строки.
 // А затем:
 // - Выведи на экран третью строку в неизменном виде;
@@ -213,9 +244,22 @@ console.log(strText.length);
 // строка номер раз
 // ```
 
-// ### 21. Задача
+// let num1 = prompt("Первая строка:");
+// let num2 = prompt("Вторая строка:");
+// let num3 = prompt("Третья строка:");
+// console.log(num3);
+// console.log(num2.toUpperCase());
+// console.log(num1.toLowerCase());
+
+
+
+// ### 21. Задача +
 // Запросить у пользователя 2 числа
 // Сначала сложить эти два числа, а затем результат умножить на 3
 // Вывести в консоль в красивом виде: <br>
 
 // `(10 + 20) × 3 = 90`
+
+// let num1 = Number(prompt("Введите первое число :"));
+// let num2 = Number(prompt("Введите первое число :"));
+// console.log("(" + num1 + " + " + num2 + ") × 3 = " + (num1 + num2) * 3);
