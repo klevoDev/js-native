30f058f2b8d8ea240fa1691546d16f94630787e9
Romcha Levedev <lr.mos@mail.ru>
2022-05-25 17:42:50 +0300

    add Alex fix

--- js/7-practice.js
+++ js/7-practice.js
@@ -7,11 +7,22 @@
 // - цикл while;
 // - цикл do-while;
 
-// let offer = "";
+
 // for (let i = 0; i < 5; i++) {
-//     offer = "Повтроим?)"
-//     console.log(offer);
+//     console.log("Повтроим?)");
 // }
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Повтроим?)");
+// }
+
+// for (let i = 5; i >= 1; i--) {
+//     console.log("Повтроим?)");
+// }
+// for (let i = 10; i >= 5; i--) {
+//     console.log("Повтроим?)");
+// }
+
+
 
 // let offer = 5;
 // while (offer < 10) {
@@ -42,6 +53,20 @@
 // console.log(result);
 // debugger
 
+// star = '*';
+// result = "";
+
+// cycle
+// 1 iter
+// result = result + star
+// result = "" + "*"
+
+// result = "*";
+
+// 2 iter
+// result = "*" + "*"
+// result = "**";
+
 
 
 // ### 3. Задача
@@ -70,10 +95,10 @@
 
 
 //### 4. Задача
-// Вывести в консоль, пользуйтесь переменной star:
+// Вывести в консоль, пользуйтесь переменной circle:
 
 // ```
-// const star = '*';
+// const circle = 'O';
 // ```
 
 // ```
@@ -84,58 +109,70 @@
 // ```
 
 
-// const star = "0";
+// const star = "O";
 // let result = "";
 // for (let i = 0; i < 7; i++) {
 //     result += star;
-//     if (result === "00") {
-//         continue
-//     } else if (result === "0000") {
-//         continue
-//     } else if (result === "000000") {
+//     if (i % 2 !== 0) {
 //         continue
 //     }
-//     result += star; // тут поставил склеивание, но только до двух доходил 00?
+//     console.log(result);
+// }
+
+// const star = "O";
+// let result = star;
+// let countLine = 4
+// for (let i = 0; i < countLine; i++) {
+//     if (i === 0) {
+//         console.log(result);
+//         continue;
+//     }
+
+//     result = `${result}${star}${star}`;
 //     console.log(result);
 // }
 
 
 
 // ### 5. Задача
-// Вывести числа в консоль из промежутка от 1 до 20 вкл.
+// Вывести числа в консоль в столбик из промежутка от 1 до 20 вкл.
 
-// let num = "";
-// for (let i = 1; i < 21; i++) {
-//     num += i;
-//     debugger
+
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i);
 // }
-// console.log(num);
-// debugger
+
+
 
 
 
 // ### 6. Задача
 // Вывести числа в консоль из промежутка от 3 до 15 не вкл.
 
-// let num = "";
 // for (let i = 3; i < 15; i++) {
-//     num += i;
-//     //debugger
+//     console.log(i);
 // }
-// console.log(num);
-// //debugger
+
 
 
 // ### 7. Задача
 // Вывести числа в консоль из промежутка от 1 до 10 вкл., доклеивая к чётным числам: `even`.
 
-// let result = "";
-// for (let i = 1; i < 11; i++) {
-//     result = i;
+
+// for (let i = 1; i <= 20; i++) {
+//     let result = i;
 //     if (i % 2 === 0) {
-//         result += " even";
+//         result += "even";
 //     }
 //     console.log(result);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i + "even");
+//     } else {
+//         console.log(i);
+//     }
 // }
 
 
@@ -143,26 +180,25 @@
 // ### 8. Задача
 // Вывести числа в консоль из промежутка от 1 до 10 вкл., доклеивая каждому третьему: `=x3=`.
 
-// let result = "";
-// for (let i = 1; i < 11; i++) {
-//     result = i;
+// for (let i = 1; i <= 10; i++) {
 //     if (i % 3 === 0) {
-//         result += "=x3=";
+//         console.log(i + "=x3=");
+//     } else {
+//         console.log(i);
 //     }
-//     console.log(result);
 // }
+
 
 
 // ### 9. Задача
 // Вывести числа в консоль из промежутка от 1 до 10 вкл., доклеивая к нечётному числу: ` - odd`.
 
-// let result = "";
-// for (let i = 1; i < 11; i++) {
-//     result = i;
+// for (let i = 1; i <= 10; i++) {
 //     if (!(i % 2 === 0)) {
-//         result += " - odd=";
+//         console.log(i + " - odd=");
+//     } else {
+//         console.log(i);
 //     }
-//     console.log(result);
 // }
 
 
@@ -170,11 +206,10 @@
 // ### 10. Задача
 // Вывести числа в консоль из промежутка от 1 до 10 вкл., доклеивая каждому второму: `=x2=`.
 
-// let result = "";
 // for (let i = 1; i < 11; i++) {
-//     result = i;
 //     if (i % 2 === 0) {
-//         result += "=x2=`";
+//         console.log(i + "=x2=");
+//     } else {
+//         console.log(i);
 //     }
-//     console.log(result);
 // }
