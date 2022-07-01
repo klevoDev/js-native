84f3c36d1a6e0a4f1298bdf0aefab5877a419723
Romcha Levedev <lr.mos@mail.ru>
2022-07-01 19:59:01 +0300

    9-practice, added 1-10 tasks

---
+++ js/9-practice.js
@@ -0,0 +1,150 @@
+// 1. Задача +
+
+// Написать функцию, которая выводит сообщение Hello world! 5 раз.
+
+// function helloWorld() {
+//     for (let i = 0; i < 5; i++)
+//         console.log("Hello world!");
+// }
+
+// helloWorld()
+
+
+
+// 2. Задача +
+
+// Написать функцию, на вход которой подаётся переменная, функция возвращает название типа данных.
+
+
+// function showType(value) {
+//     return typeof value;
+// }
+
+// let test = showType(34)
+// console.log(test);
+
+
+
+// 3. Задача +
+
+// Написать функцию, которая проверяет, является ли переменная типом string
+
+// function checkString(value) {
+//     if (typeof value === "string") {
+//         console.log("string")
+//     } else {
+//         console.log("Это " + typeof value);
+//     }
+// }
+
+// let test = 34;
+// checkString(test)
+
+
+
+// 4. Задача +
+
+// Написать функцию, которая проверяет, является ли переменная типом null
+
+// function checkNull(value) {
+//     if (value === null) {
+//         console.log("Это null")
+//     } else {
+//         console.log("Это " + typeof value);
+//     }
+// }
+
+// let test = null;
+// checkNull(test)
+
+
+
+// 5. Задача +
+
+// Написать функцию, на вход которой подаётся 2 переменных, функция проверяет является ли первая переменная типом number, а вторая - string. (Т.е. функция возвращает true или false)
+
+// function chekValue(valOne, valTwo) {
+//     if (typeof valOne === "number" && typeof valTwo === "string") {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// let test = chekValue(1, "sf")
+// console.log(test);
+
+
+
+// 6. Задача +
+
+// Написать функцию, на вход которой подаётся 2 числа, если эти числа равны 15, то возвращается true иначе false.
+
+// function checkSum(num1, num2) {
+//     if (num1 + num2 === 15) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+
+// let test = checkSum(7, 8)
+// console.log(test);
+
+
+
+// 7. Задача +
+
+// Написать функцию, на вход которой подаётся 2 числа, если первое число равно 5, а второе 3, то возвращается true иначе false
+
+// function chekValue(num1, num2) {
+//     if (num1 === 5 && num2 === 3) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// let test = chekValue(5, 3)
+// console.log(test);
+
+
+
+
+// 8. Задача +
+
+// Написать функцию, которое проверяет равно ли переданное число 10.
+
+// function chekValue(num) {
+//     return num === 10 && true
+// }
+
+// let test = chekValue(10)
+// console.log(test);
+
+
+
+// 9. Задача +
+
+// Написать функцию, которая проверяет переданное число на чётность.
+
+// function checkEven(num) {
+//     return num % 2 === 0
+// }
+
+// let test = checkEven(0)
+// console.log(test);
+
+
+
+// 10. Задача +
+
+// Написать функцию, которая проверяет переданное число на то, делится ли оно на 10 или нет.
+
+// function checkValue(num) {
+//     return num % 10 === 0
+// }
+
+// let test = checkValue(11)
+// console.log(test);
