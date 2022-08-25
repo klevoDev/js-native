7859eab1bef1cee6648667b5fd1e70f23ceb1a87
Romcha Levedev <lr.mos@mail.ru>
2022-08-25 19:01:59 +0300

    added calculator 2.0

--- js/calculate.js
+++ js/calculate.js
@@ -24,7 +24,7 @@
 
 // •	При вводе пользователем строки, не соответствующей одной из вышеописанных арифметических операций приложение бросить error и завершает свою работу. + -
 
-// ====
+// =====================================================================
 
 // переводим к int римские цифры
 const romanToInt = function (s) {
@@ -40,47 +40,15 @@ const romanToInt = function (s) {
     return num;
 };
 
-// =====
 
-// теперь результат переводим в римские цифры 
-function convertToRoman(number) {
-    let roman = {
-        "M": 1000,
-        "CM": 900,
-        "D": 500,
-        "CD": 400,
-        "C": 100,
-        "XC": 90,
-        "L": 50,
-        "XL": 40,
-        "X": 10,
-        "IX": 9,
-        "V": 5,
-        "IV": 4,
-        "I": 1
-    };
-    let res = "";
-
-    for (var i of Object.keys(roman)) {
-        var repeat = Math.floor(number / roman[i]);
-        number -= repeat * roman[i];
-        res += i.repeat(repeat);
-    }
-    return res
-}
-
-// ====
-
-
-
-
-//===============================
+//====
 
 
 const mathExpression1 = "II - I";
 const mathExpression2 = "9*9";
 const mathExpression3 = "X * X";
-const mathExpression4 = "1 +   4";
+const mathExpression4 = "1 + 1";
+
 
 
 function calculate(string) {
@@ -91,14 +59,8 @@ function calculate(string) {
     let result = "";
     let stringSplit = ''
 
-    // разбивка элементов вибираем способ (1+3 или 1 + 3)
-    if (string.length === 3) {
-        stringSplit = string.split('')
-    } else {
-        stringSplit = string.split(' ')
-    }
-
-
+    // разбивка по символьно
+    stringSplit = string.split('')
 
     if (string.length === 1) {
         throw new Error('Uncorrected action ONE');
@@ -118,18 +80,25 @@ function calculate(string) {
     }
 
     // a присваиваем
+
+    // делаем временную переменную для a
+    let temporaryVariableA = ''
+
     for (const el of stringSplit) {
         if (el !== "+" && el !== "-" && el !== "/" && el !== "*") {
-            a += el
+            temporaryVariableA += el
         } else {
             break
         }
     }
-    // a = stringSplit.slice(0, 1)
-    // console.log(a);
+    // а теперь отрезаем пробелы во временной переменной
+    for (const el of temporaryVariableA) {
+        if (el != " ") {
+            a += el
+        }
+    }
 
 
-    // 6. <string>.slice(start, end); // Возвращает новую строку, копируя в неё все символы с позиции start до позиции end НЕ ВКЛЮЧИТЕЛЬНО
 
     // присваиваем оператор
     for (const el of stringSplit) {
@@ -140,12 +109,12 @@ function calculate(string) {
         }
     }
 
-    // operator = stringSplit.slice(1, 2)
-    // console.log(operator);
+
 
     //теперь b присваиваем
     const arrEl = string.split('').reverse().join('');
     let elementReverse = ''
+    let temporaryVariableB = ''
 
     for (const el of arrEl) {
         if (el !== "+" && el !== "-" && el !== "/" && el !== "*") {
@@ -153,7 +122,16 @@ function calculate(string) {
         } else {
             break
         }
-        b = elementReverse.split('').reverse().join('')
+        temporaryVariableB = elementReverse.split('').reverse().join('')
+    }
+
+
+
+    // b теперь отрезаем пробелы во временной переменной
+    for (const el of temporaryVariableB) {
+        if (el != " ") {
+            b += el
+        }
     }
 
 
@@ -166,15 +144,12 @@ function calculate(string) {
         throw new Error('Uncorrected action BBB');
     }
 
-    // удаляем пробелы, а то NAN на b
-    b = b.trim()
-
 
     // проверка на int + римское
-    if (!isNaN(b) && isNaN(a)) {
+    if (isNaN(a) && !isNaN(b)) {
         throw new Error('Uncorrected action isNaN');
     } else if (!isNaN(a) && isNaN(b)) {
-        throw new Error('Uncorrected action isNaN')
+        throw new Error('Uncorrected action isNaN');
     }
 
 
@@ -187,6 +162,8 @@ function calculate(string) {
         let aInt = romanToInt(a)
         let bInt = romanToInt(b)
 
+
+
         // проверка от 1 до 10 вкл roman
         if (aInt > 10 || aInt < 1) {
             throw new Error('Uncorrected action BBB');
@@ -197,8 +174,6 @@ function calculate(string) {
         }
 
         // считаем
-
-
         if (Number(aInt) && Number(bInt)) {
             const count = {
                 '+': (aInt, bInt) => parseInt(aInt) + parseInt(bInt),
@@ -211,9 +186,38 @@ function calculate(string) {
 
 
         // проверка на 0 и отрицательные числа для римских чисел
-        a < b && (result = '')
+        // a < b && (result = '')
+        if ((operator === "-" || operator === "/") && a < b) {
+            result = ''
+        }
 
         // теперь результат переводим в римские цифры convertToRoman(number)
+
+        function convertToRoman(number) {
+            let roman = {
+                "M": 1000,
+                "CM": 900,
+                "D": 500,
+                "CD": 400,
+                "C": 100,
+                "XC": 90,
+                "L": 50,
+                "XL": 40,
+                "X": 10,
+                "IX": 9,
+                "V": 5,
+                "IV": 4,
+                "I": 1
+            };
+            let res = "";
+
+            for (var i of Object.keys(roman)) {
+                var repeat = Math.floor(number / roman[i]);
+                number -= repeat * roman[i];
+                res += i.repeat(repeat);
+            }
+            return res
+        }
 
         // в консоль
         result = convertToRoman(result)
@@ -223,6 +227,7 @@ function calculate(string) {
         let aInt = a
         let bInt = b
 
+        let value = ''
         if (Number(aInt) && Number(bInt)) {
             const count = {
                 '+': (aInt, bInt) => parseInt(aInt) + parseInt(bInt),
@@ -231,7 +236,17 @@ function calculate(string) {
                 '/': (aInt, bInt) => aInt / bInt,
             };
             // округляем и приводим к стринг
-            result = String(Math.round(count[operator](aInt, bInt)))
+
+
+            // result = String(Math.round(count[operator](aInt, bInt)))
+            value = String((count[operator](aInt, bInt)))
+        }
+
+        // для получения 0 (2/4)
+        if (value === "0.5") {
+            result = String(Math.floor(value))
+        } else {
+            result = String(Math.round(value))
         }
     }
     // прод
@@ -242,7 +257,7 @@ function calculate(string) {
 }
 
 
-
+// =====================================================================
 console.log(calculate(mathExpression1));
 console.log(calculate(mathExpression2));
 console.log(calculate(mathExpression3));
@@ -253,15 +268,127 @@ console.log(calculate(mathExpression4));
 
 
 
-function calculateRef(str) {
-    // 1 - Разбить строку на a, p & sign
-    // 2 - проверка параметров a, b, sign
-    // 3 - Проверить римские числа или арабские
-    // 4 - Если римские, то преобразуем в арабские
-    // 5 - Вычисляем
-    // 6 - Если были римские, то преобразовать обратно
-    // 7 - Вернуть строку - ответ (return)
-}
+// function calculateRef(str) {
+// 1 - Разбить строку на a, p & sign
+// 2 - проверка параметров a, b, sign
+// 3 - Проверить римские числа или арабские
+// 4 - Если римские, то преобразуем в арабские
+// 5 - Вычисляем
+// 6 - Если были римские, то преобразовать обратно
+// 7 - Вернуть строку - ответ (return)
+// }
+
+
+
+
+
+calculate('1 + 2'); // '3' +
+calculate('1+2'); // '3' +
+calculate(' 1  +    2 '); // '3' +
+calculate('VI / III'); // 'II' +
+calculate('VII / III'); // 'II' +
+calculate('I + II'); // 'III' +
+calculate('I - II'); // '' +
+// calculate('I + 1'); // throws Error +
+// calculate('I'); // throws Error +
+// calculate('1 + 1 + 1'); // throws Error +
+
+// Процесс отладки и тестирования:
+// Должен работать с десятичными числами (сложение)
+calculate('1 + 1')//.toBe('2'); +
+calculate('1 + 2')//.toBe('3'); +
+calculate('1+2')//.toBe('3'); +
+calculate('4 + 3')//.toBe('7'); +
+calculate('10 + 10')//.toBe('20'); +
+
+// Должен работать с десятичными числами (вычитание)
+calculate('10 - 1') //.toBe('9'); +
+calculate('5-4') //.toBe('1'); +
+calculate('4 - 4') //.toBe('0'); +
+calculate('1 - 10') //.toBe('-9'); +
+calculate('4 - 5') //.toBe('-1'); +
+
+// Должен работать с десятичными числами (умножение)
+calculate('10 * 10') //.toBe('100'); +
+calculate('4*10') //.toBe('40'); +
+calculate('5 * 1') //.toBe('5'); +
+calculate('5 * 5') //.toBe('25'); +
+
+
+// Должен работать с десятичными числами (деление)
+calculate('10 / 1') //.toBe('10'); +
+calculate('6/2') //.toBe('3'); +
+calculate('5 / 4') //.toBe('1'); +
+calculate('2 / 4') //.toBe('0'); +
+
+// Должен работать с римскими числами (сложение)
+calculate('I + I') //.toBe('II'); +
+calculate('I + II') //.toBe('III'); +
+calculate('IV + III') //.toBe('VII'); +
+calculate('X+X') //.toBe('XX'); +
+calculate('X + IX') //.toBe('XIX'); +
+
+// Должен работать с римскими числами (вычитание)
+calculate('X - I') //.toBe('IX'); +
+calculate('V - IV') //.toBe('I'); +
+calculate('IV - IV') //.toBe(''); +
+calculate('I - X') //.toBe(''); +
+calculate('IV - V') //.toBe(''); +
+
+// Должен работать с римскими числами (умножение)
+calculate('X * X') //.toBe('C'); +
+calculate('IV * X') //.toBe('XL'); +
+calculate('V * I') //.toBe('V'); +
+calculate('V * V') //.toBe('XXV'); +
+
+// Должен работать с римскими числами (деление)
+calculate('X / I') //.toBe('X'); +
+calculate('VI / II') //.toBe('III'); +
+calculate('V / IV') //.toBe('I'); +
+calculate('II / IV') //.toBe(''); +
+
+//  Должен выбрасывать ошибку на некорректных данных
+// calculate('') //.toThrowError();
+// calculate(' ') //.toThrowError(); +
+// calculate('     ') //.toThrowError(); +
+// calculate('4') //.toThrowError(); +
+// calculate('+') //.toThrowError(); +
+// calculate('++1') //.toThrowError(); +
+// calculate('V') //.toThrowError(); +
+// calculate('3 % 4') //.toThrowError(); +
+// calculate('1 + 1 + 1') //.toThrowError(); +
+// calculate('11 + 1') //.toThrowError(); +
+// calculate('1 + 11') //.toThrowError(); +
+// calculate('XI + I') //.toThrowError(); +
+// calculate('I + XI') //.toThrowError(); +
+// calculate('1 + V') //.toThrowError(); +
+// calculate('I + 1') //.toThrowError(); +
+// calculate('5 / 0') //.toThrowError(); +
+// calculate('0 + 1') //.toThrowError(); +
+// calculate('1 + 0') //.toThrowError(); +
+
+
+
+
+
+
+
+// function romain2arabic(string) {
+//     let arrSimvol = string.split(/[+*\/-]/g)
+
+//     let arrNum = []
+//     for (let i = 0; i < arrSimvol.length; i++) {
+//         let res = 0
+//         arrSimvol[i].replace(/[MDLV]|C[MD]?|X[CL]?|I[XV]?/g, function (i) {
+//             res += { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, III: 3, II: 2, I: 1 }[i]
+//         })
+//         arrNum.push(res)
+//     }
+//     return arrNum
+// }
+
+
+
 
 
 
@@ -275,7 +402,7 @@ function calculateRef(str) {
 // calculate('I + II'); // 'III' +
 // calculate('I - II'); // '' +
 // calculate('I + 1'); // throws Error +
-// calculate('I'); // throws Error + 
+// calculate('I'); // throws Error +
 // calculate('1 + 1 + 1'); // throws Error +
 
 // Процесс отладки и тестирования:
